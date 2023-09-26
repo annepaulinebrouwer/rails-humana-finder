244de28886c85a488fd9e453417bab2ac16d5904
Paula Brouwer <freelance.brouwer@gmail.com>
2023-09-26 18:15:56 +0200

    Add banner and category card to the shop index page

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,15 +1,32 @@
 class ShopsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
   before_action :set_shop, only: [:show]
+  before_action :shop_params, only: [:create]
 
   def show
   end
 
   def index
+    @shops = Shop.all
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      render :show, status: :created
+    else
+      flash[:alert] = "Couldn't create the shop"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_shop
     @shop = Shop.find(params[:id])
+  end
+
+  def shop_params
+    params.require(:shop).permit(:name, :street, :zipcode, :city, :neighbourhood, :popup)
   end
 end
